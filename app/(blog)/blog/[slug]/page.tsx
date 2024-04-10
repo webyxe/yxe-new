de9cbe5a9a5a0f1cc4a90c7829c0addb4ebb6965
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-04-10 17:57:54 -0300

    Arquivo Atualizado

--- app/(blog)/blog/[slug]/page.tsx
+++ app/(blog)/blog/[slug]/page.tsx
@@ -1,25 +1,7 @@
-import ContentArtigosSingle from "@/app/dados/contentArtigosSingle";
+import SingleSlug from "@/app/parts/blog/singleSlug";
 import { LoadingSinglePost } from "@/app/parts/estrutura/loading";
-import parse from 'html-react-parser';
+
 import { Suspense } from "react";
-
-export async function SingleSlug({ params }: any) {
-
-    const paramsSlug = params.slug;
-    const artigoCurrent = await ContentArtigosSingle({ paramsSlug });
-
-    if (artigoCurrent) {
-        return (
-            <div className="w-full md:w-2/3 lg:w-3/4">
-                <div className="">
-                    <h1 className='text-2xl mb-4'>{artigoCurrent.data.title}</h1>
-                    {parse(artigoCurrent.data.content)}
-                </div>
-            </div>
-        )
-    }
-    return null;
-}
 
 export default function PageBlogSingle({ ...props }: any) {
     return (
---
+++ app/parts/blog/singleSlug.tsx
@@ -0,0 +1,20 @@
+import ContentArtigosSingle from "@/app/dados/contentArtigosSingle";
+import parse from 'html-react-parser';
+
+export default async function SingleSlug({ params }: any) {
+
+    const paramsSlug = params.slug;
+    const artigoCurrent = await ContentArtigosSingle({ paramsSlug });
+
+    if (artigoCurrent) {
+        return (
+            <div className="w-full md:w-2/3 lg:w-3/4">
+                <div className="">
+                    <h1 className='text-2xl mb-4'>{artigoCurrent.data.title}</h1>
+                    {parse(artigoCurrent.data.content)}
+                </div>
+            </div>
+        )
+    }
+    return null;
+}

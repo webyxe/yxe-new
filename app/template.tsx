385a160fd935804b8020f965e394ff9b432c6a9a
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-04-12 14:03:17 -0300

    Contato Create - API

--- app/template.tsx
+++ app/template.tsx
@@ -1,11 +1,7 @@
-"use client"
-
-import { usePathname } from "next/navigation";
 import Footer from "./parts/estrutura/footer";
 import Header from "./parts/estrutura/header";
 
 const Template = ({children}:any) => {
-    const isHome = usePathname();
     return(
         <>
             <Header />

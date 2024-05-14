72a0afd79f128f95bdb66496959467da5285ee3e
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-05-14 18:43:27 -0300

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,30 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "300", "500", "600", "700", "900"] });
 
 export const metadata: Metadata = {
-  title: "Agência YXE",
-  description: "Tudo em um só lugar",
+    title: "Agência YXE",
+    description: "Tudo em um só lugar",
 };
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={poppins.className}>
+                <Script id="google-analytics">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-5G77JQERLY');
+                `}
+                </Script>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5G77JQERLY" />
+                {children}
+            </body>
+        </html>
+    );
 }
